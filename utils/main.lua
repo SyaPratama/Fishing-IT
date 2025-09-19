8db0faa4a286c88df5d663d369952f69be170bac
SyaPratama <rasyaputraq17@gmail.com>
2025-09-20 02:51:15 +0700

    chore: test speed 0.1 auto fishing

--- utils/main.lua
+++ utils/main.lua
@@ -115,7 +115,7 @@ function AutoFishing()
     while ActiveAutoFishing do
         pcall(CastFishingRod)
         if not ActiveAutoFishing then break end
-        task.wait(0.5)
+        task.wait(0.1)
     end
     print("🛑 Auto fishing stopped")
 
